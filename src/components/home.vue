138ae5ef065aafa005a46a7cf1f63d5f909c8402
zhaokewei <zhaokewei1992@gmail.com>
2020-05-16 17:30:04 +0800

    源数据也可以输入时、分、秒。

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,9 +1,6 @@
 <template xmlns:http="http://www.w3.org/1999/xhtml">
     <div>
         <calcute-frame></calcute-frame>
-        <div>
-            友情链接: <a href="http://yaofan.zhaokewei.com">Surprise</a>
-        </div>
     </div>
 </template>
 
